7eb2cb5e9855e2ad64425a3fdd8421f0af290777
Kevin Dodge <kevin.dodge@wgu.edu>
2023-10-12 09:19:15 -0600

    test: Test showing the bug

--- src/test/java/test_email_validation/Test_email_valudationTest.java
+++ src/test/java/test_email_validation/Test_email_valudationTest.java
@@ -1,20 +1,40 @@
 package test_email_validation;
 
-import io.micronaut.runtime.EmbeddedApplication;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import jakarta.inject.Inject;
-import org.junit.jupiter.api.Assertions;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 @MicronautTest
+@WireMockTest()
 class Test_email_valudationTest {
-
-    @Inject
-    EmbeddedApplication<?> application;
+    @RegisterExtension
+    static WireMockExtension downloadWm = WireMockExtension.newInstance().build();
 
     @Test
-    void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+    void test_sucess(RequestSpecification spec) {
+        spec.given()
+                .contentType(ContentType.JSON)
+                .get("/myobj/123")
+                .then()
+                .log().ifValidationFails(LogDetail.ALL)
+                .statusCode(200);
+
+    }
+
+    @Test
+    void test_notFound(RequestSpecification spec) {
+        spec.given()
+                .contentType(ContentType.JSON)
+                .get("/myobj/err")
+                .then()
+                .log().ifValidationFails(LogDetail.ALL)
+                .statusCode(404);
+
     }
 
 }
